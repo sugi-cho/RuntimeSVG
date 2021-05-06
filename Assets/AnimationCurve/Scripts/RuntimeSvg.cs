698678e4086042dda145eaf5721266c83dd02bc9
sugino <git@sugi.cc>
2021-05-07 02:22:26 +0900

    VectorUtils.TessellatePath

--- Assets/AnimationCurve/Scripts/RuntimeSvg.cs
+++ Assets/AnimationCurve/Scripts/RuntimeSvg.cs
@@ -11,16 +11,13 @@ public class RuntimeSvg : MonoBehaviour
     [SerializeField] string assetpath;
     [SerializeField] ViewportOptions viewPrtOptions;
     [SerializeField] SVGParser.SceneInfo sceneInfo;
-    [SerializeField] VectorUtils.TessellationOptions tessellationOptions;
-    [SerializeField] List<VectorUtils.Geometry> geometries;
-    public Vector2[] vertices;
+    public Vector2[] path;
 
     // Start is called before the first frame update
     void Start()
-    { 
+    {
         using (var stream = new StreamReader(assetpath))
             sceneInfo = SVGParser.ImportSVG(stream, viewPrtOptions);
-        Debug.Log(sceneInfo.Scene.Root.Children.Count);
         var children = sceneInfo.Scene.Root.Children;
         children.ForEach(c =>
         {
@@ -31,17 +28,30 @@ public class RuntimeSvg : MonoBehaviour
             });
         });
 
-        tessellationOptions = new VectorUtils.TessellationOptions { StepDistance = 10, MaxCordDeviation = 5f, MaxTanAngleDeviation = 10, SamplingStepSize = 0.5f };
-        geometries = VectorUtils.TessellateScene(sceneInfo.Scene, tessellationOptions);
-        Debug.Log(geometries.Count);
-
-        var g = geometries[0];
-        vertices = g.Indices.Select(idx => g.Vertices[idx]).ToArray();
+        var tessellateOptions = new VectorUtils.TessellationOptions { StepDistance = 10, MaxCordDeviation = 10f, MaxTanAngleDeviation = 10, SamplingStepSize = 0f };
+        var pathProps = new PathProperties { Corners = PathCorner.Tipped, Head = PathEnding.Chop, Tail = PathEnding.Chop, Stroke = new Stroke { Color = Color.white } };
+        var indeces = new ushort[0];
+        VectorUtils.TessellatePath(sceneInfo.Scene.Root.Children[0].Shapes[0].Contours[0], pathProps, tessellateOptions, out path, out indeces);
+        var center = new Vector2(path.Average(v2 => v2.x), path.Average(v2 => v2.y));
+        path = path.Select(p => p - center).Distinct().ToArray();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.matrix = transform.localToWorldMatrix;
+        for (var i = 0; i < path.Length - 1; i++)
+        {
+            Gizmos.color = Color.white;
+            Gizmos.DrawLine(path[i], path[i + 1]);
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(path[i], 1f);
+            Gizmos.DrawSphere(path[i + 1], 1f);
+        }
     }
 }
